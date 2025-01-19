754d5d5db8e03580dc5d3ec0abf4620a4c29d58c
shubz2911 <shubhamsura1998@gmail.com>
2025-01-19 17:54:02 +0530

    Trying

---
+++ Array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A[5];
+    A[0]=34;
+    A[1]=45;
+    A[3]=89;
+    cout<<A[2]<<endl;
+    cout<<A[1]<<endl;
+    int B[5]={2,3,4,5};
+    cout<<B[4];
+    return 0;
+}
